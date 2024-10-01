ac0dd37a5cbec93799a895481c845db9e75f7d9c
Kencho5 <giokenchadze@gmail.com>
2024-10-01 10:14:49 +0400

    Added: fonts

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import { AuthProvider } from "./auth/AuthContext";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 //import { useNavigation } from "react-router-dom";
 //import TopBarProgress from "react-topbar-progress-indicator";
 
@@ -15,13 +16,18 @@ const queryClient = new QueryClient({
 
 function Layout() {
   //const navigation = useNavigation();
+  const { i18n } = useTranslation();
+  const language = i18n.language;
+  const fontClass = language === "ge" ? "font-geo" : "font-eng";
 
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <Navbar />
-        {/* navigation.state == "loading" && <TopBarProgress /> */}
-        <Outlet />
+        <div className={fontClass}>
+          <Navbar />
+          {/* navigation.state == "loading" && <TopBarProgress /> */}
+          <Outlet />
+        </div>
       </QueryClientProvider>
     </AuthProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      geo: ["SF-Geo", "sans-serif"],
+      eng: ["GEOMedium", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
