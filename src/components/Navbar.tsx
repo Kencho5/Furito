91d281d4582e95c00dfb7e8e3fcc902cae30fdaa
Kencho5 <giokenchadze@gmail.com>
2024-10-01 10:49:40 +0400

    Added: colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-stone-800">
+    <nav className="border-b border-neutral-200 bg-neutral-100">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex">
