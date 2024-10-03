f0d04383ce47f05ff9d4da5f0c585a7253f15aa4
Kencho5 <giokenchadze@gmail.com>
2024-10-03 11:51:00 +0400

    Removed: sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://staging.furito.com",
-      dynamicRoutes: ["/", "/login", "/register"],
-    }),
-  ],
+  plugins: [react()],
 });
